1c610dfe31b9628509a660a48cce6c32147d0fdb
Sagar Deshmukh <sagarsdeshmukh@users.noreply.github.com>
2025-10-07 12:55:35 +0530

    Update timber functions as per Timber2 syntax

--- author.php
+++ author.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying Author Archive pages
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
+ * Methods for \Timber\Helper can be found in the /lib sub-directory
  *
  * @package  WordPress
  * @subpackage  Timber
@@ -15,7 +15,7 @@ use Timber\PostQuery;
 wp_register_script( 'load_more', get_template_directory_uri() . '/assets/js/load_more.js', [ 'jquery', 'main' ], '0.0.1', true );
 wp_enqueue_script( 'load_more' );
 
-$context = Timber::get_context();
+$context = Timber::context();
 
 $post_args = [
 	'posts_per_page' => 10,
@@ -30,7 +30,7 @@ $idea_args = [
 ];
 
 if ( isset( $wp_query->query_vars['author'] ) ) {
-	$author              = new P4_User( $wp_query->query_vars['author'] );
+	$author              = Timber::get_user( $wp_query->query_vars['author'] );
 	$context['author']   = $author;
 	$context['title']    = 'Author Archives: ' . $author->name();
 	$post_args['author'] = $wp_query->query_vars['author'];
